8607d211b9a9ce56e6a77e63a3ee56cf89a37385
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-04-12 09:34:43 -0600

    Add NewTour component to create and display tour information

--- src/components/NewTour.tsx
+++ src/components/NewTour.tsx
@@ -1,7 +1,69 @@
-import React from 'react'
+'use client'
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+import {
+  createNewTour,
+  generateTourResponse,
+  getExistingTour,
+} from '@/utils/actions';
+import { TourInfo } from './TourInfo';
 
 export const NewTour = () => {
+  const { mutate, isPending, data: tour } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination);
+      if (newTour) {
+        return newTour;
+      }
+      toast.error('No matching city found...');
+      return null;
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    mutate(destination);
+  };
+
+  if (isPending) {
+    return <span className='loading loading-lg'></span>;
+  }
   return (
-    <div>NewTour</div>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className=' mb-4'>Select your dream destination</h2>
+        <div className='join w-full'>
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='city'
+            name='city'
+            required
+          />
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='country'
+            name='country'
+            required
+          />
+          <button
+            className='btn btn-primary join-item'
+            type='submit'
+            disabled={isPending}
+          >
+            {isPending ? 'please wait...' : 'generate tour'}
+          </button>
+        </div>
+      </form>
+      <div className='mt-16'>
+        <div className='mt-16'>{tour ? <TourInfo tour={tour} /> : null}</div>
+      </div>
+    </>
+  );
+};
